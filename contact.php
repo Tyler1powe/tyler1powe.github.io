6cfe1e14c175257cfe2a80c6c68ac76fbeec4b2e
Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>
2024-01-09 19:01:01 -0600

    Update contact.php
    Signed-off-by: Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>

--- contact.php
+++ contact.php
@@ -1,20 +1,19 @@
 <?php
 
 if (isset($_POST['submit'])) {
-$name = $_POST['name'];
-  $mailform = $_POST['mail'];
-  $numberform = $_POST['phone'];
-  $message = $_POST['message'];
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail']; // corrected variable name
+    $numberForm = $_POST['phone']; // corrected variable name
+    $message = $_POST['message'];
 
-  $mailTo = "tylerlpowe@gmail.com";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have a message."".$name".\n\n".$message'.$numberform;
+    $mailTo = "tylerlpowe@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have a message.\n\nName: ".$name."\nEmail: ".$mailFrom."\nPhone: ".$numberForm."\n\n".$message;
 
-  mail($mailTo, $name, $txt, $headers);
+    mail($mailTo, "Message from ".$name, $txt, $headers);
 
-  header("Location: index.html?MessageSent");
-
-
+    header("Location: index.html?MessageSent");
+    exit(); // add exit to stop further execution
 }
-  
+
 ?>
