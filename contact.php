7b2ae4c8b4a7c5a16bb4189b6783b5e4d7c44453
Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>
2024-03-05 16:40:58 -0600

    Update contact.php
    Signed-off-by: Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>

--- contact.php
+++ contact.php
@@ -1,19 +1,22 @@
+
 <?php
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $numberForm = $_POST['phone']; 
-    $message = $_POST['message'];
-
-    $mailTo = "tylerlpowe@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have a message.\n\nName: ".$name."\nEmail: ".$mailFrom."\nPhone: ".$numberForm."\n\n".$message;
-
-    mail($mailTo, "Message from ".$name, $txt, $headers);
-
-    header("Location: index.html?MessageSent");
-    exit(); 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+    
+    $to = "tylerlpowe@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message";
+    
+    // Send email
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: Message not sent.";
+    }
+} else {
+    echo "Error: Invalid request.";
 }
-
 ?>
