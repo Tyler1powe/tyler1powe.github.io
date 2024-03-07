1d6f7109386bd361ee6dcd8290ef9ab5d7580c16
Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>
2024-03-07 13:44:10 -0600

    Update scripts.js
    Signed-off-by: Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,8 +1,1 @@
 
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-        <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // Activate the form with SB Forms JS
-        SB.initFormSubmit('#contactForm');
-    });
-</script>
